78347ad4a0e314fd30e2c19b3a257e648529c9de
RomainBoucard <rom.boucard@gmail.com>
2022-12-07 16:20:05 +0100

    Ajout fichier main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   // printf() displays the string inside quotation
+   std::cout << "Hello world !\n" << std::endl;
+
+   return 0;
+}
